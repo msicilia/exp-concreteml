e090692840403b8ddef1163cec60e9f74f9856aa
Miguel-Angel Sicilia <msicilia@gmail.com>
2025-03-01 12:23:32 +0100

    Added comparison with regular sklean models

--- gen_models.py
+++ gen_models.py
@@ -1,7 +1,9 @@
 
 MODELS_DIR = "./pickle_field"
 from sklearn.metrics import accuracy_score
-from concrete.ml.sklearn import DecisionTreeClassifier, RandomForestClassifier
+from concrete.ml.sklearn import DecisionTreeClassifier as FHEDecisionTreeClassifier, RandomForestClassifier as FHERandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import load_breast_cancer
 from itertools import product
 from sklearn.datasets import make_classification
@@ -25,30 +27,41 @@ def experiment(modelname: str):
         X, y = make_classification(random_state=42, n_features=n_features, n_samples=1_000)
         X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
         if modelname == "DecisionTreeClassifier":
-            model = DecisionTreeClassifier(max_depth=max_depth, n_bits=n_bits)
+            model = DecisionTreeClassifier(max_depth=max_depth)
         elif modelname == "RandomForestClassifier":
-            model = RandomForestClassifier(n_estimators=10, max_depth=max_depth, n_bits=n_bits)
+            model = RandomForestClassifier(n_estimators=10, max_depth=max_depth)
+        elif modelname == "FHEDecisionTreeClassifier":
+            model = FHEDecisionTreeClassifier(max_depth=max_depth, n_bits=n_bits)
+        elif modelname == "FHERandomForestClassifier":
+            model = FHERandomForestClassifier(n_estimators=10, max_depth=max_depth, n_bits=n_bits)
         tic = time.perf_counter()
         model.fit(X,y)
         toc = time.perf_counter()
         train_time = toc - tic
         print(f"Training time: {train_time}")
+
+        if modelname.startswith("FHE"):
+            tic = time.perf_counter()
+            model.compile(X_train)
+            toc = time.perf_counter()
+            compilation_time = toc - tic
+            print(f"Compilation time: {compilation_time}")
+
         tic = time.perf_counter()
-        model.compile(X_train)
-        toc = time.perf_counter()
-        compilation_time = toc - tic
-        print(f"Compilation time: {compilation_time}")
-        tic = time.perf_counter()
-        y_pred = model.predict(X_test, fhe="execute")
+        if modelname.startswith("FHE"):
+            y_pred = model.predict(X_test, fhe="execute")
+        else:
+            y_pred = model.predict(X_test)
         toc = time.perf_counter()
         prediction_time = toc - tic
         print(f"Prediction time: {prediction_time}")
+
         #with open(f"{MODELS_DIR}/{model}_{max_depth}_{n_bits}_{n_features}.pkl", "wb") as f:
         #    pickle.dump(model, f) 
         print(f"model name: {model}, max_depth: {max_depth}, n_bits: {n_bits}, n_features: {n_features}")
         acc = accuracy_score(y_test, y_pred)
         print(f"Accuracy: {acc}")
-        new_row = {"model": model, "max_depth": max_depth, "n_bits": n_bits, 
+        new_row = {"model": modelname, "max_depth": max_depth, "n_bits": n_bits, 
                     "n_features": n_features, 
                     "training_time": train_time, 
                 "compilation_time": compilation_time, 
@@ -59,5 +72,5 @@ def experiment(modelname: str):
 
 
 if __name__ == '__main__':
-    with Pool(2) as p:
-        print(p.map(experiment, ["DecisionTreeClassifier", "RandomForestClassifier"]))
+    with Pool(4) as p:
+        print(p.map(experiment, ["DecisionTreeClassifier", "RandomForestClassifier", "FHEDecisionTreeClassifier", "FHERandomForestClassifier"]))
--- hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from exp-concreteml!")
-
-
-if __name__ == "__main__":
-    main()
