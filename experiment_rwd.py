cb5573ee524371ee6fcad5f933b259e6a8b678be
Miguel-Angel Sicilia <msicilia@gmail.com>
2025-03-01 11:55:32 +0100

    Added multiprocessing

---
+++ experiment_rwd.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from concrete.ml.sklearn import DecisionTreeClassifier, RandomForestClassifier
+
+
+
+
+data = pd.read_csv("xxxxx.csv")
